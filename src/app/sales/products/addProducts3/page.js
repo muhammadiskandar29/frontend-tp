5daae19654e2d243d1e8caaa41aa3d82ea396d12
Muhammad Iskandar <muhammad.21iskandar@gmail.com>
2025-12-29 00:40:41 +0700

    Detailing Customers

--- src/app/sales/products/addProducts3/page.js
+++ src/app/sales/products/addProducts3/page.js
@@ -1056,7 +1056,7 @@ export default function AddProducts3Page() {
                       showIcon
                       showTime
                       hourFormat="24"
-                      dateFormat="dd/mm/yy HH:mm"
+                      dateFormat="dd/mm/yy"
                       timeOnly={false}
                       showSeconds={false}
                       showButtonBar
